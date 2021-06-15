e664d85c37a6c6948144a62a676281af74981373
Natalie Finnegan <nfinneg2@gmail.com>
2021-06-15 13:21:16 -0600

    component folders, API/utils, googlefonts, header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 //import "App.css";
+import API from "./utils/API";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Testing</p>
+        <Header />
       </header>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container heading">
+        <h1 class="display-4">React Employee Directory</h1>
+        <p class="lead">See all current employee information in aggregate</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
