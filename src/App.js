55beaa7ce6028f704310ea9b1231ee92d94bc1a1
Natalie Finnegan <nfinneg2@gmail.com>
2021-06-16 21:14:22 -0600

    still working on results

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import Footer from "./components/Footer/Footer";
 import empDirectory from "./components/Directory";
 
 function App() {
-  useEffect(() => {
-    // function getEmps() {
-    fetch("https://randomuser.me/api/?results=20")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log({ data });
-      });
-    // }
-    //getEmps();
-    console.log("All done");
-  }, []);
+  // useEffect(() => {
+  //   // function getEmps() {
+  //   fetch("https://randomuser.me/api/?results=20")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log({ data });
+  //     });
+  //   // }
+  //   //getEmps();
+  //   console.log("All done");
+  // }, []);
 
   return (
     <div className="App">
       <Header />
-      {/* <Directory /> */}
+      <Directory />
       <Footer />
     </div>
   );
--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,82 +1,125 @@
-import MUIDataTable from "mui-datatables";
+import React, { useEffect, useState } from "react";
 
-const empDirectory = () => {
+const EmpDirectory = () => {
+  const [employees, setEmployees] = useState();
+
   let allEmps = [];
-  let filteredEmps = [];
   let newEmps;
-  //   fetch("https://randomuser.me/api/?results=20")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       // console.log({ data });
-  //       const { results } = data;
-  //       let allEmps = results;
+  let filteredEmps = [];
 
-  for (let employee of allEmps) {
-    //console.log(employee);
-    newEmps = [
-      {
-        id: Math.floor(Math.random() * 1000),
-        name: `${employee.name.first} ${employee.name.last}`,
-        email: employee.email,
-        phone: employee.cell,
-      },
-    ];
+  useEffect(() => {
+    getEmps();
+    console.log("all done");
+  }, []);
 
-    filteredEmps.push(...newEmps);
-  }
-  //console.log(filteredEmps);
-
-  //console.log(filteredEmps);
-
-  const columns = [
-    {
-      name: "id",
-      label: "Emp-ID",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-    {
-      name: "name",
-      label: "Name",
-      options: {
-        filter: true,
-        sort: true,
-      },
-    },
-    {
-      name: "email",
-      label: "Email",
-      options: {
-        filter: true,
-        sort: false,
-      },
-    },
-    {
-      name: "phone",
-      label: "Cell Phone",
-      options: {
-        filter: false,
-        sort: false,
-      },
-    },
-  ];
-
-  const options = {
-    filter: true,
-    filterType: "checkbox",
+  const getEmps = () => {
+    fetch("https://randomuser.me/api/?results=20")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log({ data });
+        const { results } = data;
+        let allEmps = results;
+        console.log(allEmps);
+        for (let employee of allEmps) {
+          console.log(employee);
+          newEmps = [
+            {
+              id: Math.floor(Math.random() * 1000),
+              name: `${employee.name.first} ${employee.name.last}`,
+              email: employee.email,
+              phone: employee.cell,
+            },
+          ];
+          filteredEmps.push(...newEmps);
+          console.log(filteredEmps);
+        }
+        return filteredEmps;
+      })
+      .then((...filteredEmps) => {
+        setEmployees(filteredEmps);
+        console.log(filteredEmps);
+      })
+      .catch((err) => console.log(err));
   };
-
-  // });
-  return (
-    <MUIDataTable
-      title={"Employee Directory"}
-      data={newEmps}
-      columns={columns}
-      options={options}
-    />
-  );
+  console.log(filteredEmps);
+  return <div>test</div>;
 };
 
-export default empDirectory;
+export default EmpDirectory;
+
+//   let allEmps = [];
+//   let filteredEmps = [];
+//   let newEmps;
+//   fetch("https://randomuser.me/api/?results=20")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // console.log({ data });
+//       const { results } = data;
+//       let allEmps = results;
+
+//   for (let employee of allEmps) {
+//     //console.log(employee);
+//     newEmps = [
+//       {
+//         id: Math.floor(Math.random() * 1000),
+//         name: `${employee.name.first} ${employee.name.last}`,
+//         email: employee.email,
+//         phone: employee.cell,
+//       },
+//     ];
+
+//  filteredEmps.push(...newEmps);
+//   }
+//console.log(filteredEmps);
+
+//console.log(filteredEmps);
+
+//   const columns = [
+//     {
+//       name: "id",
+//       label: "Emp-ID",
+//       options: {
+//         filter: true,
+//         sort: false,
+//       },
+//     },
+//     {
+//       name: "name",
+//       label: "Name",
+//       options: {
+//         filter: true,
+//         sort: true,
+//       },
+//     },
+//     {
+//       name: "email",
+//       label: "Email",
+//       options: {
+//         filter: true,
+//         sort: false,
+//       },
+//     },
+//     {
+//       name: "phone",
+//       label: "Cell Phone",
+//       options: {
+//         filter: false,
+//         sort: false,
+//       },
+//     },
+//   ];
+
+//   const options = {
+//     filter: true,
+//     filterType: "checkbox",
+//   };
+
+// });
+//   return (
+//     <MUIDataTable
+//       title={"Employee Directory"}
+//       data={newEmps}
+//       columns={columns}
+//       options={options}
+//     />
+//   );
